65298754801d91280f04dd000d0024d6df54c2d2
calle123 <cj.adolfsson@gmail.com>
2012-09-17 20:22:52 +0200

    försäker göra en branch

--- src/com/Grupp01/gymapp/MainActivity.java
+++ src/com/Grupp01/gymapp/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //testkommentar
     }
 
     @Override
