bd5ecaac0c7720bbda310471839470b4feb5137b
calle123 <cj.adolfsson@gmail.com>
2012-09-25 09:29:11 +0200

    fixed the dialog and made some small changes

--- src/com/Grupp01/gymapp/Ovningar.java
+++ src/com/Grupp01/gymapp/Ovningar.java
@@ -1,6 +1,8 @@
 package com.Grupp01.gymapp;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
@@ -19,5 +21,13 @@ public class Ovningar extends SherlockActivity {
     	MenuInflater inflater = getSupportMenuInflater();
     	inflater.inflate(R.menu.ovningar, menu);
         return true;
+    }
+    
+    public void newExercise(View view)
+    {
+    	Dialog dialog = new Dialog(this);
+    	dialog.setContentView(R.layout.dialog);
+    	dialog.setTitle("New Exercise");
+    	dialog.show();
     }
 }
