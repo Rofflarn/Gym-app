8753df9addd6b67f9cd7b8ecd3164b4ed2e91a83
Anders Gustafsson <anders.johan.gustafsson77@gmail.com>
2012-09-27 14:42:30 +0200

    Added databasetables, 6 tables totalt

--- src/com/Grupp01/gymapp/Database.java
+++ src/com/Grupp01/gymapp/Database.java
@@ -20,11 +20,45 @@ public class Database {
 		@Override
 		public void onCreate(SQLiteDatabase db)
 		{
-			db.execSQL("CREATE TABLE " + "Exercises" + " (" + "ExceriseId" + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-		               + "ExcersiePri")
+			createExercises(db);
+			createMeasureDataTypes(db);
+			createExerciseTypes(db);
+			createPasses(db);
+			createMeasures(db);
+			createMeasureData(db);			 
 		}
 		
+		private void createExercises(SQLiteDatabase db)
+		{
+			db.execSQL("CREATE TABLE Exercises (ExceriseId INTEGER PRIMARY KEY AUTOINCREMENT, ExercisePri INTEGER, ExerciseSec INTEGER," +
+					" ExerciseName TEXT NOT NULL, ExerciseDesc TEXT, ExersiceNote Text, ExerciseSportId INTEGER, ExerciseTypeId INTEGER NOT NULL);");	
+		}
 		
+		private void createExerciseTypes(SQLiteDatabase db)
+		{
+			db.execSQL("CREATE TABLE ExerciseTypes (ExerciseTypeId INTEGER PRIMARY KEY AUTOINCREMENT, ExerciseTypeName TEXT);");
+		}
+		
+		private void createMeasureDataTypes(SQLiteDatabase db)
+		{
+			db.execSQL("CREATE TABLE MeasureDataTypes (MeasureDataTypeId INTEGER PRIMARY KEY AUTOINCREMENT, MeasureDataTypeName Text);");
+		}
+		
+		private void createMeasureData(SQLiteDatabase db)
+		{
+			db.execSQL("CREATE TABLE MeasureData (MeasureDataId INTEGER PRIMARY KEY AUTOINCREMENT, MeasureData REAL NOT NULL, " +
+						"MeasureDataTypeId INTEGER NOT NULL, MeasureId INTEGER NOT NULL);");
+		}
+				
+		private void createPasses(SQLiteDatabase db)
+		{
+			db.execSQL("CREATE TABLE Passes (PassID INTEGER PRIMARY KEY AUTOINCREMENT, PassTime NUMERIC NOT NULL, Passname TEXT, UserId INTEGER NOT NULL);");
+		}
+		
+		private void createMeasures(SQLiteDatabase db)
+		{
+			db.execSQL("CREATE TABLE Measures (MeasureId INTEGER PRIMARY KEY AUTONINCREMENT, MeasureData NUMERIC NOT NULL, UserId INTEGER NOT NULL);");
+		}
 	}
 
 }
