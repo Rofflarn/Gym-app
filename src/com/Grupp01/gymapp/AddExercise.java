a91cdfbed709060d8d00288484bca35abaeaf6b6
joelolofsson <joel.olofsson@gmail.com>
2012-09-30 17:12:04 +0200

    added spinners, updated activity_add_exercise.xml

--- src/com/Grupp01/gymapp/AddExercise.java
+++ src/com/Grupp01/gymapp/AddExercise.java
@@ -1,14 +1,10 @@
 package com.Grupp01.gymapp;
 
-import org.xml.sax.ContentHandler;
-
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -16,12 +12,11 @@ import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 
-public class AddExercise extends SherlockActivity implements OnItemSelectedListener {
-	private TextView comment;
-	private Button button;
+public class AddExercise extends SherlockActivity implements AdapterView.OnItemSelectedListener {
+	private TextView tvPMuscle,tvSMuscle, tvSport;
 	private String[] items;
-	private Spinner spinnerType;
-	private ContentHandler CH;
+	private Spinner spinnerType, spinnerPMuscle, spinnerSMuscle, spinnerSport;
+
 	//private Spinner typeOfTraining;
 	//private static final String[] items={"Cardio", "Static", "Dynamic", "blaj"};
 
@@ -46,69 +41,85 @@ public class AddExercise extends SherlockActivity implements OnItemSelectedListe
 		inflater.inflate(R.menu.activity_add_exercise, menu);
 		return true;
 	}
-	
+
 	public void setUpInvisLayout(){
-		button = (Button) findViewById(R.id.buttontest);
-		button.setVisibility(View.INVISIBLE);
-		
-		comment = (TextView) findViewById(R.id.comment);
-		comment.setVisibility(View.INVISIBLE);
+		spinnerPMuscle = (Spinner) findViewById(R.id.spinner_primary_muscle);
+		spinnerPMuscle.setVisibility(View.INVISIBLE);
+
+		spinnerSMuscle = (Spinner) findViewById(R.id.spinner_secondary_muscle);
+		spinnerSMuscle.setVisibility(View.INVISIBLE);
+
+		spinnerSport = (Spinner) findViewById(R.id.spinner_sport);
+		spinnerSport.setVisibility(View.INVISIBLE);
+
+		tvPMuscle = (TextView) findViewById(R.id.textView_primary_muscle);
+		tvPMuscle.setVisibility(View.INVISIBLE);
+
+		tvSMuscle = (TextView) findViewById(R.id.textView_secondary_muscle);
+		tvSMuscle.setVisibility(View.INVISIBLE);
+
+		tvSport = (TextView) findViewById(R.id.textView_sport);
+		tvSport.setVisibility(View.INVISIBLE);
+
 	}	
-		
-		
-		
-	
+
+
+
+
 	public void initSpinnerType()
 	{
 		spinnerType = (Spinner) findViewById(R.id.spinner_type_of_training);
 		// Create an ArrayAdapter using the string array and a default spinner layout
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-		        R.array.trainingtype_array, android.R.layout.simple_spinner_item);
+				R.array.trainingtype_array, android.R.layout.simple_spinner_item);
 		// Specify the layout to use when the list of choices appears
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// Apply the adapter to the spinner
 		spinnerType.setAdapter(adapter);
-		
-		//spinnerType = (Spinner) findViewById(R.id.spinner_type_of_training);
-		//spinnerType.setOnItemSelectedListener(this);
-		
+
+		spinnerType = (Spinner) findViewById(R.id.spinner_type_of_training);
+		spinnerType.setOnItemSelectedListener(this);
+
 	}
-	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-			System.out.println("Entering onItemSelected");
+
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {		
 		if(items[position].equals("Static"))
 		{
-			setContentView(R.layout.add_exercise_static);
-			spinnerType = (Spinner)findViewById(R.id.spinner_type_of_training);
-			initSpinnerType();
-			spinnerType.setSelection(1);
-			System.out.println("static");
-			
-			//button.setVisibility(View.VISIBLE);
-		}
-		
-		else if(items[position].equals("Dynamic"))
-		{
-			setContentView(R.layout.add_exercise_static);
-			spinnerType = (Spinner)findViewById(R.id.spinner_type_of_training);
-			initSpinnerType();
-			spinnerType.setSelection(2);
-			System.out.println("dynamic");
-		}
-		else
-		{
-			setContentView(R.layout.add_exercise_static);
-			System.out.println("cardio");
-			spinnerType.setSelection(1);
-			button.setVisibility(view.VISIBLE);
-			initSpinnerType();
-			//setContentView(R.layout.activity_add_exercise);
-	
+			setUpInvisLayout();
+			spinnerPMuscle.setVisibility(View.VISIBLE);
+			spinnerSMuscle.setVisibility(View.VISIBLE);
+			tvSMuscle.setVisibility(View.VISIBLE);
+			tvPMuscle.setVisibility(View.VISIBLE);
+
+
+
+			System.out.println("static");			//button.setVisibility(View.VISIBLE);
 		}
 
-		
+		else if(items[position].equals("Dynamic"))
+		{
+			setUpInvisLayout();
+			spinnerPMuscle.setVisibility(View.VISIBLE);
+			spinnerSMuscle.setVisibility(View.VISIBLE);
+			tvSMuscle.setVisibility(View.VISIBLE);
+			tvPMuscle.setVisibility(View.VISIBLE);
+			System.out.println("dynamic");
+		}
+		else if (items[position].equals("Cardio"))
+		{
+			System.out.println("cardio");
+			setUpInvisLayout();
+			spinnerSport.setVisibility(View.VISIBLE);
+			tvSport.setVisibility(View.VISIBLE);
+
+			//setContentView(R.layout.activity_add_exercise);
+
+		}
+
+
 	}
 
 	public void onNothingSelected(AdapterView<?> parent) {
-		
+
 	}
 }
