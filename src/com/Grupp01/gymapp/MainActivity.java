55e5d81153a9a442d31176f9c896e27868b12a77
Anders Gustafsson <anders.johan.gustafsson77@gmail.com>
2012-09-30 12:01:45 +0200

    Added Imagebuttons to mainMenu.

--- src/com/Grupp01/gymapp/MainActivity.java
+++ src/com/Grupp01/gymapp/MainActivity.java
@@ -3,12 +3,15 @@ package com.Grupp01.gymapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-import com.actionbarsherlock.view.*;
+import android.widget.ImageButton;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
 
 public class MainActivity extends SherlockActivity {
+	
+	//private ImageButton button1 = (ImageButton) findViewById(R.id.button1);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,7 @@ public class MainActivity extends SherlockActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getSupportMenuInflater();
     	inflater.inflate(R.menu.activity_main, menu);
+    	
         return true;
     }
     
