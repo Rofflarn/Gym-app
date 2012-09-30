1cdfead347059e4ecc4f2b52fbf47abc27fe95c7
calle123 <cj.adolfsson@gmail.com>
2012-09-30 13:20:49 +0200

    refaktorerat code

--- gen/com/Grupp01/gymapp/R.java
+++ gen/com/Grupp01/gymapp/R.java
@@ -821,21 +821,20 @@ containing a value of this type.
         public static final int abs__title=0x7f04001b;
         public static final int abs__titleDivider=0x7f04001c;
         public static final int abs__up=0x7f04000b;
-        public static final int button=0x7f040034;
+        public static final int add_Button=0x7f040035;
         public static final int button1=0x7f04002b;
         public static final int button2=0x7f04002c;
         public static final int button3=0x7f04002d;
         public static final int button4=0x7f04002e;
         public static final int buttontest=0x7f04002a;
+        public static final int cancel_Button=0x7f040034;
         public static final int comment=0x7f040029;
         public static final int disableHome=0x7f040009;
         public static final int exerciseName=0x7f040033;
         public static final int homeAsUp=0x7f040006;
-        public static final int linearLayout1=0x7f040036;
+        public static final int linearLayout1=0x7f040038;
         public static final int listMode=0x7f040002;
-        public static final int listView1=0x7f040035;
-        public static final int menu_addExe=0x7f040039;
-        public static final int menu_settings=0x7f040038;
+        public static final int menu_settings=0x7f04003a;
         public static final int normal=0x7f040001;
         public static final int selection=0x7f040032;
         public static final int showCustom=0x7f040008;
@@ -846,8 +845,10 @@ containing a value of this type.
         public static final int spinner_type_of_training=0x7f040028;
         public static final int tabMode=0x7f040003;
         public static final int textView1=0x7f040027;
-        public static final int textView2=0x7f040037;
+        public static final int textView2=0x7f040039;
         public static final int textView_type_of_training=0x7f04002f;
+        public static final int theList=0x7f040036;
+        public static final int theList_rows=0x7f040037;
         public static final int useLogo=0x7f040004;
         public static final int wrap_content=0x7f040000;
     }
@@ -884,7 +885,8 @@ containing a value of this type.
         public static final int sherlock_spinner_dropdown_item=0x7f03001a;
         public static final int sherlock_spinner_item=0x7f03001b;
         public static final int statistik=0x7f03001c;
-        public static final int workout=0x7f03001d;
+        public static final int thelist_row=0x7f03001d;
+        public static final int workout=0x7f03001e;
     }
     public static final class menu {
         public static final int activity_add_exercise=0x7f0c0000;
--- src/com/Grupp01/gymapp/Ovningar.java
+++ src/com/Grupp01/gymapp/Ovningar.java
@@ -1,12 +1,16 @@
 package com.Grupp01.gymapp;
 
+import java.util.ArrayList;
+
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,66 +19,81 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
 
-public class Ovningar extends SherlockActivity {
+public class Ovningar extends SherlockActivity implements OnMenuItemClickListener {
+	
+	public final static String EXTRA_EXERCISE_NAME = "com.Grupp01.gymapp.message";
+	private Dialog dialog;
+	private Intent intent;
+	private com.actionbarsherlock.view.MenuItem newExButton;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ovningar);
-        getSupportActionBar().setHomeButtonEnabled(true);
+        dialog = new Dialog(this);
+        intent = new Intent(this, AddExercise.class);
+        createListView();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
     	MenuInflater inflater = getSupportMenuInflater();
     	inflater.inflate(R.menu.ovningar, menu);
-    	com.actionbarsherlock.view.MenuItem item = menu.add("New exercise");
-    	item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-    	final Dialog dialog = new Dialog(this);
-    	final Intent intent = new Intent(this, AddExercise.class);
-    	item.setOnMenuItemClickListener(new OnMenuItemClickListener()
-    	{
+    	newExButton = menu.add("New exercise");
+    	newExButton.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+    	newExButton.setOnMenuItemClickListener(this);
+        return true;
+    }
+    
+    public void createListView()
+    {
+    	ListView listView = (ListView)findViewById(R.id.theList);
+    	ArrayList<String> listElements = new ArrayList<String>();
+    	listElements.add("ett");
+    	listElements.add("två");
+    	listElements.add("tre");
+    	listElements.add("fyra");
+    	listElements.add("fem");
+    	listElements.add("sex");
+    	listElements.add("sju");
+    	ArrayAdapter<String> element_adapter = new ArrayAdapter<String>(this, R.layout.thelist_row, listElements);
+    	listView.setAdapter(element_adapter);
+    }
 
-			@Override
-			public boolean onMenuItemClick(com.actionbarsherlock.view.MenuItem item)
-			{
+	@Override
+	public boolean onMenuItemClick(com.actionbarsherlock.view.MenuItem item) {
+		
+		//if(item.getActionView() == newExButton)
+		//{
+
 		    	dialog.setContentView(R.layout.dialog);
 		    	dialog.setTitle("New Exercise");
-		    	Button button = (Button) dialog.findViewById(R.id.button);
-		    	button.setOnClickListener(
+		    	Button cancel_Button = (Button) dialog.findViewById(R.id.cancel_Button);
+		    	Button add_Button = (Button) dialog.findViewById(R.id.add_Button);
+		    	add_Button.setOnClickListener(
 		    			new OnClickListener()
 		    			{
 		    				public void onClick(View view)
 		    				{
+		    					intent.putExtra(EXTRA_EXERCISE_NAME, ((EditText) dialog.findViewById(R.id.exerciseName)).getText().toString());
+		    					dialog.dismiss();
 		    					startActivity(intent);
 		    				}
 		    			});
+		    	
+		    	cancel_Button.setOnClickListener(
+		    			new OnClickListener()
+		    			{
+							@Override
+							public void onClick(View view)
+							{
+								dialog.dismiss();
+							}
+		    				
+		    			});
 		    	dialog.show();
 				return false;
-			}
-    		
-    	});
-        return true;
-    }
-    
-//    public void newExercise(View view)
-//    {
-//    	final Dialog dialog = new Dialog(this);
-//    	dialog.setContentView(R.layout.dialog);
-//    	dialog.setTitle("New Exercise");
-//    	
-//    	Button button = (Button) dialog.findViewById(R.id.button);
-//    	button.setOnClickListener(
-//    			new OnClickListener()
-//    			{
-//    				public void onClick(View view)
-//    				{
-//    					TextView tv = (TextView) findViewById(R.id.test);
-//    					EditText et = (EditText) dialog.findViewById(R.id.exerciseName);
-//    					tv.setText(et.getText().toString());
-//    					dialog.dismiss();
-//    				}
-//    			});
-//    	dialog.show();
-//    }
+		//}
+		//return false;
+	}
 }
