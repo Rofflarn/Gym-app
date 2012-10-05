31f0bf8d4582004e01d0f9844f009c7e6ce77a8c
erikny <skrotan@gmail.com>
2012-10-05 11:23:06 +0200

    Added licence information in java files

--- src/com/Grupp01/gymapp/RegisterCardioActivity.java
+++ src/com/Grupp01/gymapp/RegisterCardioActivity.java
@@ -1,4 +1,22 @@
+/*This file is part of Gymapp.
+*
+*   Gymapp is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   Gymapp is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*  along with Gymapp.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
 package com.Grupp01.gymapp;
+
 
 import java.util.ArrayList;
 
@@ -19,9 +37,9 @@ import com.actionbarsherlock.view.MenuItem;
  * @peer reviewed by
  * @date 04/10/12
  *
-* Class RegisterCardioActivity is an activity that enables the user to
-* register his or hers result when performing a cardio exercise.
-* The user will be able to input time (minutes and seconds) and the distance. 
+ * Class RegisterCardioActivity is an activity that enables the user to
+ * register his or hers result when performing a cardio exercise.
+ * The user will be able to input time (minutes and seconds) and the distance. 
  * 
  * <p>This class i a part of the </p><i>View</i><p> package, and a part of the </p><i>Workout</i>
  * <p> Subpackage</p> 
