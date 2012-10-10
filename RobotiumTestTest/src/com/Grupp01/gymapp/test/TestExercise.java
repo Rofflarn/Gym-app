2ade0f5ab897b46c5990d43ac21de7fa4e84bbe2
calle123 <cj.adolfsson@gmail.com>
2012-10-10 16:25:29 +0200

    added testcode for exercise activity

--- RobotiumTestTest/src/com/Grupp01/gymapp/test/TestExercise.java
+++ RobotiumTestTest/src/com/Grupp01/gymapp/test/TestExercise.java
@@ -17,9 +17,134 @@ public class TestExercise extends ActivityInstrumentationTestCase2<MainActivity>
 		solo = new Solo(getInstrumentation(), getActivity());
 	}
 	
-	public void testButton()
+	public void testViewAllExercises()
 	{
 		solo.clickOnImageButton(1);
+	}
+	
+	public void testExerciseHomeButton()
+	{
+		solo.clickOnImageButton(1);
+		solo.clickOnActionBarHomeButton();
+	}
+	
+	public void testEnterNameNewExercise()
+	{
+		solo.clickOnImageButton(1);
+		solo.clickOnMenuItem("ADD EXCERSICE!");
+		solo.enterText(0, "Exercise Name");
+		solo.clickOnButton(1);
+	}
+	
+	public void testAddExerciseMinimalSettingsCardio()
+	{
+		solo.clickOnImageButton(1);
+		solo.clickOnMenuItem("ADD EXCERSICE!");
+		solo.enterText(0, "Exercise Name");
+		solo.clickOnButton(1);
+		solo.pressSpinnerItem(0, 0);
+		solo.pressSpinnerItem(1, 1);
+		solo.clickOnButton(0);
+	}
+	
+	public void testAddExerciseMinimalSettingsStatic()
+	{
+		solo.clickOnImageButton(1);
+		solo.clickOnMenuItem("ADD EXCERSICE!");
+		solo.enterText(0, "Exercise Name");
+		solo.clickOnButton(1);
+		solo.pressSpinnerItem(0, 1);
+		solo.pressSpinnerItem(1, 1);
+		solo.pressSpinnerItem(2, 1);
+		solo.clickOnButton(0);
+	}
+	
+	public void testAddExerciseMinimalSettingsDynamic()
+	{
+		solo.clickOnImageButton(1);
+		solo.clickOnMenuItem("ADD EXCERSICE!");
+		solo.enterText(0, "Exercise Name");
+		solo.clickOnButton(1);
+		solo.pressSpinnerItem(0, 2);
+		solo.pressSpinnerItem(1, 1);
+		solo.pressSpinnerItem(2, 1);
+		solo.clickOnButton(0);
+	}
+	
+	public void testAddExerciseMaximalSettingsCardio()
+	{
+		solo.clickOnImageButton(1);
+		solo.clickOnMenuItem("ADD EXCERSICE!");
+		solo.enterText(0, "Exercise Name");
+		solo.clickOnButton(1);
+		solo.pressSpinnerItem(0, 0);
+		solo.pressSpinnerItem(1, 1);
+		solo.enterText(0, "test");
+		solo.enterText(1, "test");
+		solo.clickOnButton(0);
+	}
+	
+	public void testAddExerciseMaximalSettingsStatic()
+	{
+		solo.clickOnImageButton(1);
+		solo.clickOnMenuItem("ADD EXCERSICE!");
+		solo.enterText(0, "Exercise Name");
+		solo.clickOnButton(1);
+		solo.pressSpinnerItem(0, 1);
+		solo.pressSpinnerItem(1, 1);
+		solo.pressSpinnerItem(2, 1);
+		solo.enterText(0, "test");
+		solo.enterText(1, "test");
+		solo.clickOnButton(0);
+	}
+	
+	public void testAddExerciseMaximalSettingsDynamic()
+	{
+		solo.clickOnImageButton(1);
+		solo.clickOnMenuItem("ADD EXCERSICE!");
+		solo.enterText(0, "Exercise Name");
+		solo.clickOnButton(1);
+		solo.pressSpinnerItem(0, 2);
+		solo.pressSpinnerItem(1, 1);
+		solo.pressSpinnerItem(2, 1);
+		solo.enterText(0, "test");
+		solo.enterText(1, "test");
+		solo.clickOnButton(0);
+	}
+	
+	public void testDeleteExercise()
+	{
+		solo.clickOnImageButton(1);
+	}
+	
+	public void testEditExercise()
+	{
+		solo.clickOnImageButton(1);
+		solo.clickInList(0);
+		solo.enterText(0, "Edit");
+		solo.enterText(1, "Edit");
+		solo.clickOnButton(0);
+	}
+	
+	public void testRenameExercise()
+	{
+		solo.clickOnImageButton(1);
+	}
+	
+	public void testAbortingAddExercise()
+	{
+		solo.clickOnImageButton(1);
+		solo.clickOnMenuItem("ADD EXCERSICE!");
+		solo.enterText(0, "Exercise Name");
+		solo.clickOnButton(1);
+		solo.clickOnButton(1);
+	}
+	
+	public void testAbortingEditExercise()
+	{
+		solo.clickOnImageButton(1);
+		solo.clickInList(0);
+		solo.clickOnButton(1);
 	}
 
 	protected void tearDown() throws Exception {
