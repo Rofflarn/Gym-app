be6062234f842569290da87c43634ddd36a27232
calle123 <cj.adolfsson@gmail.com>
2012-10-10 13:42:11 +0200

    setup some file for robotium test

--- RobotiumTestTest/src/com/Grupp01/gymapp/test/TestExercise.java
+++ RobotiumTestTest/src/com/Grupp01/gymapp/test/TestExercise.java
@@ -3,19 +3,27 @@ package com.Grupp01.gymapp.test;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.Grupp01.gymapp.MainActivity;
+import com.jayway.android.robotium.solo.Solo;
 
 public class TestExercise extends ActivityInstrumentationTestCase2<MainActivity> {
 
+	private Solo solo;
 	public TestExercise() {
 		super("com.Grupp01.gymapp", MainActivity.class);
 	}
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+	
+	public void testButton()
+	{
+		solo.clickOnImageButton(1);
 	}
 
 	protected void tearDown() throws Exception {
-		super.tearDown();
+		solo.finishOpenedActivities();
 	}
 
 }
